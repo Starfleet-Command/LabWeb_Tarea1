b5f7b4f2abccf41baac538e4d77629ae95bc94b2
Luis <luisrevilla20@gmail.com>
2021-02-22 12:37:16 -0600

    DB lista

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Album_has_Genre extends Model
+class Song extends Model
 {
     //
 }
--- database/migrations/2021_02_22_174911_create_album_genre_table.php
+++ database/migrations/2021_02_22_174911_create_album_genre_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSongsTable extends Migration
+class CreateAlbumGenreTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateSongsTable extends Migration
      */
     public function up()
     {
-        Schema::create('songs', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->integer('duration');
+        Schema::create('album_genre', function (Blueprint $table) {
             $table->unsignedBigInteger('album_id');
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
             $table->foreign('album_id')->references('id')->on('albums');
             $table->timestamps();
         });
@@ -30,6 +29,6 @@ class CreateSongsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('songs');
+        Schema::dropIfExists('album_genre');
     }
 }
